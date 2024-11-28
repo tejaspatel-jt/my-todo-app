b269ff6db9e3c731f6855fa2f6ab0923a2fe3b28
Tejas Patel <tejas.patel@agilityhealthradar.com>
2024-11-28 11:44:15 +0530

    configured "Redux DevTools" Chrome Extension in store.js file for debugging application's state changes in sexy way.

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,6 +12,10 @@ const store = configureStore({
     reducer: rootReducer, // Set the root reducer
     middleware: (getDefaultMiddleware) => 
         getDefaultMiddleware().concat(sagaMiddleware), // Add saga middleware
+
+    // Enable "Redux DevTools" Chrome Extension in non-production environment like Development
+    devTools: process.env.NODE_ENV !== 'production', 
+
 });
 
 // Run the root saga
